961c29f145f6339a15599cf7a3c6e6d9d011ce4e
Abhishekmt18 <abhiab3516@gmail.com>
2026-01-21 23:40:38 +0530

    Added user login API
    Implemented product APIs
    
    Completed cart functionality
    
    Added checkout flow with orders and transactions
    
    Refactored cart controller logic

--- src/main/java/com/securecommerce/app/controller/UserController.java
+++ src/main/java/com/securecommerce/app/controller/UserController.java
@@ -1,5 +1,40 @@
 package com.securecommerce.app.controller;
 
+
+import com.securecommerce.app.entity.User;
+import com.securecommerce.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody User loginRequest) {
+
+        User user = userRepository.findByEmail(loginRequest.getEmail());
+
+        if (user == null) {
+            return "User not found";
+        }
+
+        if (!user.getPassword().equals(loginRequest.getPassword())) {
+            return "Invalid password";
+        }
+
+        return "Login successful";
+    }
 
 }
